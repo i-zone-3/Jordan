b67c1d705170d9fbb13e31dcad812faed5091597
Jordan Thijssen <stargazerdio1999@gmail.com>
2024-07-01 00:04:19 +0100

    added c# soln

---
+++ CsharpSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using System;
+using System.Diagnostics;
+
+
+public partial class Program
+{
+    static void Main(string[] args)
+    {
+        string testConnectionResult = TestConnection();
+        Debug.WriteLine(testConnectionResult);
+        int addResult = Add(1, 2);
+        Debug.WriteLine(addResult);
+        string pbFunctionResult = PBFunction();
+        Debug.WriteLine(pbFunctionResult);
+    }
+
+    [LibraryImport("pbtest.dll", StringMarshalling = StringMarshalling.Utf16)]
+	public static partial string TestConnection();
+
+    [DllImport("pbtest.dll")]
+	public static extern string PBFunction();
+
+    [LibraryImport("pbtest.dll")]
+    public static partial int Add(int a, int b);
+
+
+}
+
